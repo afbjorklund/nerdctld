3d5c3a5a2ad1e506b4f7c6fc7443dce60702da4c
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-03-06 12:44:33 +0100

    First version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nerdctld
+
+This is a daemon offering a `nerdctl.sock` endpoint.
+
+It can be used with `DOCKER_HOST=unix://nerdctl.sock`.
+
+## Implemented commands
+
+* version
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func nerdctlVersion() string {
+	nv, err := exec.Command("nerdctl", "--version").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	v := strings.TrimSuffix(string(nv), "\n")
+	v = strings.Replace(v, "nerdctl version ", "", 1)
+	return v
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/_ping", func(c *gin.Context) {
+		c.Writer.Header().Set("API-Version", "1.24")
+		c.Writer.Header().Set("Content-Type", "text/plain")
+		c.String(http.StatusOK, "OK")
+	})
+
+	r.GET("/v1.24/version", func(c *gin.Context) {
+		var ver struct {
+			Version       string
+			APIVersion    string `json:"ApiVersion"`
+			GitCommit     string
+			GoVersion     string
+			Os            string
+			Arch          string
+			KernelVersion string `json:",omitempty"`
+			Experimental  bool   `json:",omitempty"`
+			BuildTime     string `json:",omitempty"`
+		}
+		ver.Version = nerdctlVersion()
+		ver.APIVersion = "1.24"
+		ver.GoVersion = runtime.Version()
+		ver.Os = runtime.GOOS
+		ver.Arch = runtime.GOARCH
+		ver.Experimental = true
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.JSON(http.StatusOK, ver)
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	//r.Run(":2375")
+	r.RunUnix("nerdctl.sock")
+}
