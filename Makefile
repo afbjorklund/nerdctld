0c0b4698094bea6e3301aabdc3f4d6faf9d21fe4
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-12-20 18:27:50 +0100

    nerdctld version 0.2.1

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ PREFIX ?= /usr/local
 
 all: binaries
 
-VERSION = 0.2.0
+VERSION = 0.2.1
 
 nerdctld: main.go go.mod
 	$(GO) build -o $@ $(BUILDFLAGS)
--- main.go
+++ main.go
@@ -1117,7 +1117,7 @@ func run(cmd *cobra.Command, args []string) error {
 }
 
 func version() string {
-	return "0.2.0"
+	return "0.2.1"
 }
 
 func main() {
